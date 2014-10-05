bd91902dadba10a2d0d774dde9cb8f0ed8eb7c61
Zoe Ames <zoeames@gmail.com>
2014-10-05 02:27:12 -0500

    ship page populated and button added

--- client/views/ships/ships.js
+++ client/views/ships/ships.js
@@ -2,13 +2,17 @@
   'use strict';
 
   angular.module('capstone')
-  .controller('ShipsCtrl', ['$scope', 'Ship', function($scope, Ship){
+  .controller('ShipsCtrl', ['$scope', '$location', 'Ship', function($scope, $location, Ship){
     $scope.ship = {};
     $scope.ships = [];
 
     Ship.all().then(function(response){
       $scope.ships = response.data.ships;
     });
+
+    $scope.pickShip= function(){
+      $location.path('/dashboard');
+    };
 
   }]);
 })();

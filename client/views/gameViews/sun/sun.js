78b8f75ce790420c8120cf6562b0b9fcf1487456
Zoe Ames <zoeames@gmail.com>
2014-10-10 17:24:40 -0500

    stage one complete

--- client/views/gameViews/sun/sun.js
+++ client/views/gameViews/sun/sun.js
@@ -21,7 +21,7 @@
     $scope.sunChoice = function(choice){
       switch(choice){
         case 'one':
-          var passMessage = 'Your ship travels successfully out of the solar system to begin its journey.';
+          $scope.passMessage = 'Your ship travels successfully out of the solar system to begin its journey.';
           if($scope.ship === 'Shuttlecraft'){
             $scope.speed    = 0.000000007922022; /* in ly/sec*/
           }else{
@@ -30,22 +30,45 @@
           $scope.timeSpentSec  = $scope.distance/$scope.speed;
           $scope.timeSpentYear = $scope.timeSpentSec/31536000;
           $scope.newTime       = $scope.time*1+$scope.timeSpentYear;
-          var newHealth     = $scope.health,
-              newStage      = 'sun';
-          Game.addStage($routeParams.gameId, newStage, newHealth, $scope.newTime, passMessage).then(function(response){
+          $scope.newHealth     = $scope.health;
+          $scope.newStage      = 'sun';
+          Game.addStage($routeParams.gameId, $scope.newStage, $scope.newHealth, $scope.newTime, $scope.passMessage).then(function(response){
             $location.path('/'+$routeParams.gameId+'/asteroid');
             });
+          break;
 
-          break;
         case 'two':
-          alert('button two');
+          $scope.passMessage = 'Your ship stays near the sun for a month practicing some training maneuvers.  It then travels at standard warp out of the solar system.';
+          if($scope.ship === 'Shuttlecraft'){
+            $scope.speed    = 0.000000007922022; /* in ly/sec*/
+          }else{
+            $scope.speed    = 0.00001245; /* in ly/sec*/
+          }
+          $scope.timeSpentSec  = $scope.distance/$scope.speed;
+          $scope.timeSpentYear = $scope.timeSpentSec/31536000;
+          /*30 days added for training*/
+          $scope.newTime       = $scope.time*1+$scope.timeSpentYear+30/365;
+          $scope.newHealth     = $scope.health;
+          $scope.newStage      = 'sun';
+          Game.addStage($routeParams.gameId, $scope.newStage, $scope.newHealth, $scope.newTime, $scope.passMessage).then(function(response){
+            $location.path('/'+$routeParams.gameId+'/asteroid');
+            });
           break;
+
         case 'three':
-          alert('button three');
+          $scope.passMessage = 'You take your ship in closer to the sun to study the surface.  A new ensign mistakenly hits the port thruster which drives your ship into the sun, killing the whole crew.';
+          if($scope.ship === 'Shuttlecraft'){
+            $scope.speed    = 0.000000007922022; /* in ly/sec*/
+          }else{
+            $scope.speed    = 0.00001245; /* in ly/sec*/
+          }
+          $scope.newTime       = $scope.time*1;
+          $scope.newHealth     = 0;
+          $scope.newStage      = 'sun';
+          Game.addStage($routeParams.gameId, $scope.newStage, $scope.newHealth, $scope.newTime, $scope.passMessage).then(function(response){
+            $location.path('/'+$routeParams.gameId+'/youDied');
+            });
        }
-     /* Captain.setCaptain(captainId, $routeParams.gameId).then(function(response){
-        $location.path('/'+$routeParams.gameId+'/sun');
-       });*/
     };
 
 

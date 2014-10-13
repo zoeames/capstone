af385150d9a7fe96a7760da9fd8923dec07d9bdc
Zoe Ames <zoeames@gmail.com>
2014-10-12 19:56:43 -0500

    css added to youdied page

--- client/views/youdied/youdied.js
+++ client/views/youdied/youdied.js
@@ -2,7 +2,12 @@
   'use strict';
 
   angular.module('capstone')
-  .controller('DiedCtrl', ['$scope', '$location', '$routeParams', 'Ship', function($scope, $location, $routeParams, Ship){
+  .controller('DiedCtrl', ['$scope', '$location', '$routeParams', 'Ship', 'Game', function($scope, $location, $routeParams, Ship, Game){
+    $scope.captain='';
+
+    Game.gameInfo($routeParams.gameId, 'sun').then(function(response){
+      $scope.captain = response.data.myGame.captain.name;
+    });
 
     $scope.seeStats = function(){
       $location.path('/dashboard');

2a17312f9ee0cd65d1137fea1634e84c6afcd6a2
Zoe Ames <zoeames@gmail.com>
2015-02-27 11:57:04 -0600

    testing added

---
+++ test/unit/user.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var expect    = require('chai').expect,
+    User      = require('../../server/models/user'),
+    dbConnect = require('../../server/lib/mongodb'),
+    cp        = require('child_process'),
+    Mongo     = require('mongodb'),
+    db        = 'capstone-test';
+
+describe('User', function(){
+  before(function(done){
+    dbConnect(db, function(){
+      done();
+    });
+  });
+
+  beforeEach(function(done){
+    cp.execFile(__dirname + '/../scripts/clean-db.sh', [db], {cwd:__dirname + '/../scripts'}, function(err, stdout, stderr){
+      done();
+    });
+  });
+
+  describe('constructor', function(){
+    it('should create a new User object', function(){
+      var o = {
+        username : 'sue',
+        password : 'abcd',
+        },
+      u = new User(o);
+      expect(u).to.be.instanceof(User);
+    });
+  });
+});
